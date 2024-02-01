e2e75582baee44c1e640434249a34036e68ee2be
Colin Bagley <cjbagley@googlemail.com>
2024-02-01 12:22:06 +0000

    Added Line component

---
+++ components/line.py
@@ -0,0 +1,6 @@
+from components.point import Point
+
+class Line:
+    def __init__(self, point_a: Point, point_b: Point) -> None:
+        self.__point_a = None
+        self.__point_b = None
---
+++ tests/test_line.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from components.line import Line
+from components.point import Point
+
+class TestLine(TestCase):
+    def test_line_construction(self):
+        try:
+            point_a = Point(10, 20)
+            point_b = Point(40, 20)
+            line = Line(point_a, point_b)
+        except:
+            self.fail("Line could not be constructed")
+        
+        self.assertIsInstance(line, Line)
