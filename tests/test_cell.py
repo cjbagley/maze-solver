709786b3a0c521d59d0d3c1657d5c5dbd6a24d6b
Colin Bagley <cjbagley@googlemail.com>
2024-02-02 19:05:50 +0000

    Fixing cell test

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
 from gui.cell import Cell 
+from gui.window import Window
+from gui.point import Point 
 
 class TestCell(TestCase):
     def test_cell_construction(self):
-        cell = Cell() 
+        win = Window(700, 600)
+        cell = Cell(win, Point(20, 300), Point(51, 360)) 
         self.assertIsInstance(cell, Cell)
--- tests/test_point.py
+++ tests/test_point.py
@@ -4,5 +4,5 @@ from gui.point import Point
 class TestPoint(TestCase):
     def test_point_construction(self):
        point = Point(3, 200)
-       self.assertEquals(point.x, 3)
-       self.assertEquals(point.y, 200)
+       self.assertEqual(point.x, 3)
+       self.assertEqual(point.y, 200)
